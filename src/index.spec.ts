493903ef9f6c55775f6173f225699877a528824f
Brian Kotos <brian.kotos@gmail.com>
2025-12-05 23:41:55 -0500

    It should say that I visited a directory when called with the file that I just visited

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,15 +2,25 @@ import { describe, it, expect } from 'vitest';
 import { Breadcrumbs } from './index';
 
 describe(Breadcrumbs.name, () => {
-  it('should say that I visited a directory when called with the parent directory of a file', () => {
-    // arrange
-    const directory = '/var/home/jdoe/Pictures/2022/12'
-    const photoPath = `${directory}/IMG_6532.PNG`
+  // arrange
+  const directory = '/var/home/jdoe/Pictures/2022/12'
+  const photoPath = `${directory}/IMG_6532.PNG`
 
+  it('should say that I visited a directory when called with the parent directory of a file', () => {
     // act
     const breadcrumbs = Breadcrumbs()
     breadcrumbs.visit(photoPath)
     const hasVisited = breadcrumbs.hasVisited(directory)
+
+    // assert
+    expect(hasVisited).toBe(true)
+  })
+
+  it('should say that I visited a directory when called with the file that I just visited', () => {
+    // act
+    const breadcrumbs = Breadcrumbs()
+    breadcrumbs.visit(photoPath)
+    const hasVisited = breadcrumbs.hasVisited(photoPath)
 
     // assert
     expect(hasVisited).toBe(true)
