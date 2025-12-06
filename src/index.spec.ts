92c6c09b56f7d0fc8074eae3aef0712ca2c8b7d0
Brian Kotos <brian.kotos@gmail.com>
2025-12-06 11:44:37 -0500

    Posix

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,109 +2,111 @@ import { describe, it, expect } from 'vitest';
 import { FileTrail, hydrate } from './index';
 
 describe(FileTrail.name, () => {
-  it('should say that I visited a directory when called with the parent directory of a file', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-
-    // assert
-    expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022/12')).toBe(true)
-  })
-
-  it('should say that I visited a directory when called with the file that I just visited', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-
-    // assert
-    expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')).toBe(true)
-  })
-
-  it('should say I did NOT visit a directory when called with a different directory than the directory of the file I just visited', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-
-    // assert
-    expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022/11')).toBe(false)
-  })
-
-  it('should not say that I vitied the root directory when no visits have been made', () => {
-    // act
-    const fileTrail = FileTrail()
-
-    // assert
-    expect(fileTrail.hasVisited('/')).toBe(false)
-  })
-
-  it('should say that I vitied the grandparent directory after visiting a file', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-
-    // assert
-    expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022')).toBe(true)
-  })
-
-  it('should say that I vitied root directory after visiting a file', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-
-    // assert
-    expect(fileTrail.hasVisited('/')).toBe(true)
-  })
-
-  it('should say that I did NOT complete a directory after visiting a file in it once', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-
-    // assert
-    expect(fileTrail.hasCompleted('/var/home/jdoe/Pictures/2022/12')).toBe(false)
-  })
-
-  it('should NOT say that I completed a directory after visiting two files in it', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6533.PNG')
-
-    // assert
-    expect(fileTrail.hasCompleted('/var/home/jdoe/Pictures/2022/12')).toBe(false)
-  })
-
-  it('should say that I completed a directory after visiting a file in it, and then visiting a file in a different directory', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/11/IMG_6533.PNG')
-
-    // assert
-    expect(fileTrail.hasCompleted('/var/home/jdoe/Pictures/2022/12')).toBe(true)
-  })
-
-  it('should NOT mark the root directory as completed after visiting two descendants that have different parents', () => {
-    // act
-    const fileTrail = FileTrail()
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
-    fileTrail.visit('/var/home/jdoe/Pictures/2022/11/IMG_6533.PNG')
-
-    // assert
-    expect(fileTrail.hasCompleted('/')).toBe(false)
-  })
-
-  const relativePaths = ['file.txt', '..', './file.txt', '../file.txt', '~/file.txt', '~']
-  relativePaths.forEach(path => {
-    it(`should throw an error when visiting a relative path: ${path}`, () => {
+  describe('posix', () => {
+    it('should say that I visited a directory when called with the parent directory of a file', () => {
       // act
       const fileTrail = FileTrail()
-      const act = () => {
-        fileTrail.visit(path)
-      }
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
 
       // assert
-      expect(act).toThrow('Relative paths are not currently supported. Is this something you would find value in? I would love to hear from you: https://github.com/bkotos/file-trail/issues/new')
+      expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022/12')).toBe(true)
+    })
+
+    it('should say that I visited a directory when called with the file that I just visited', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+
+      // assert
+      expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')).toBe(true)
+    })
+
+    it('should say I did NOT visit a directory when called with a different directory than the directory of the file I just visited', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+
+      // assert
+      expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022/11')).toBe(false)
+    })
+
+    it('should not say that I vitied the root directory when no visits have been made', () => {
+      // act
+      const fileTrail = FileTrail()
+
+      // assert
+      expect(fileTrail.hasVisited('/')).toBe(false)
+    })
+
+    it('should say that I vitied the grandparent directory after visiting a file', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+
+      // assert
+      expect(fileTrail.hasVisited('/var/home/jdoe/Pictures/2022')).toBe(true)
+    })
+
+    it('should say that I vitied root directory after visiting a file', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+
+      // assert
+      expect(fileTrail.hasVisited('/')).toBe(true)
+    })
+
+    it('should say that I did NOT complete a directory after visiting a file in it once', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+
+      // assert
+      expect(fileTrail.hasCompleted('/var/home/jdoe/Pictures/2022/12')).toBe(false)
+    })
+
+    it('should NOT say that I completed a directory after visiting two files in it', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6533.PNG')
+
+      // assert
+      expect(fileTrail.hasCompleted('/var/home/jdoe/Pictures/2022/12')).toBe(false)
+    })
+
+    it('should say that I completed a directory after visiting a file in it, and then visiting a file in a different directory', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/11/IMG_6533.PNG')
+
+      // assert
+      expect(fileTrail.hasCompleted('/var/home/jdoe/Pictures/2022/12')).toBe(true)
+    })
+
+    it('should NOT mark the root directory as completed after visiting two descendants that have different parents', () => {
+      // act
+      const fileTrail = FileTrail()
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/12/IMG_6532.PNG')
+      fileTrail.visit('/var/home/jdoe/Pictures/2022/11/IMG_6533.PNG')
+
+      // assert
+      expect(fileTrail.hasCompleted('/')).toBe(false)
+    })
+
+    const relativePaths = ['file.txt', '..', './file.txt', '../file.txt', '~/file.txt', '~']
+    relativePaths.forEach(path => {
+      it(`should throw an error when visiting a relative path: ${path}`, () => {
+        // act
+        const fileTrail = FileTrail()
+        const act = () => {
+          fileTrail.visit(path)
+        }
+
+        // assert
+        expect(act).toThrow('Relative paths are not currently supported. Is this something you would find value in? I would love to hear from you: https://github.com/bkotos/file-trail/issues/new')
+      })
     })
   })
 })
