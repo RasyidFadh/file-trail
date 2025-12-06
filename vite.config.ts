7f657f1d97643942d9ffb0e533b143103e519118
Brian Kotos <brian.kotos@gmail.com>
2025-12-05 21:24:20 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'breadcrumbs',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [],
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+  },
+});
